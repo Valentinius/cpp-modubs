22eac2aca3020a62cca3118572f927bdce4d5f24
Alexey Kozlovsky <63499468+AlexeyKozlovsky@users.noreply.github.com>
2022-06-30 16:25:10 +0300

    deleted useless

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UNTITLED1_INCLUDE_UTILS_H_
+#define UNTITLED1_INCLUDE_UTILS_H_
+
+namespace modbus_utils {
+
+}
+#endif //UNTITLED1_INCLUDE_UTILS_H_
---
+++ src/modbusclient.cpp
@@ -0,0 +1,5 @@
+#include "modbusclient.h"
+
+
+
+
