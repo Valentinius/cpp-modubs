0ce351c0a1bb59fa351e391c10d97fbf835a55dc
Alexey Kozlovsky <63499468+AlexeyKozlovsky@users.noreply.github.com>
2022-06-30 16:34:09 +0300

    fixed utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,13 @@
 #ifndef UNTITLED1_INCLUDE_UTILS_H_
 #define UNTITLED1_INCLUDE_UTILS_H_
 
-namespace modbus_utils {
+namespace modbus {
+  namespace enums {
 
+  }
+
+  namespace utils {
+
+  }
 }
 #endif //UNTITLED1_INCLUDE_UTILS_H_
